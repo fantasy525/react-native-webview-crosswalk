b10455521e0a7048acef4fe7b51b362879f2d722
fantasy525 <1056726491@qq.com>
2018-09-05 16:45:04 +0800

    增加部分接口

--- src/main/java/com/jordansexton/react/crosswalk/webview/CrosswalkWebViewGroupManager.java
+++ src/main/java/com/jordansexton/react/crosswalk/webview/CrosswalkWebViewGroupManager.java
@@ -2,6 +2,9 @@ package com.jordansexton.react.crosswalk.webview;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.view.ViewGroup;
 
 import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.ReadableArray;
@@ -12,16 +15,21 @@ import com.facebook.react.common.annotations.VisibleForTesting;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.annotations.ReactProp;
-import org.xwalk.core.XWalkNavigationHistory;
-import org.xwalk.core.XWalkView;
 
+import org.xwalk.core.XWalkCookieManager;
+import org.xwalk.core.XWalkNavigationHistory;
+import org.xwalk.core.XWalkSettings;
+import org.xwalk.core.XWalkView;
 import org.json.JSONObject;
 import org.json.JSONException;
 
 import com.facebook.react.views.webview.events.TopMessageEvent;
 
 import javax.annotation.Nullable;
+
+
 import java.util.Map;
+
 
 public class CrosswalkWebViewGroupManager extends ViewGroupManager<CrosswalkWebView> {
 
@@ -53,6 +61,26 @@ public class CrosswalkWebViewGroupManager extends ViewGroupManager<CrosswalkWebV
     public CrosswalkWebView createViewInstance (ThemedReactContext context) {
         Activity _activity = reactContext.getCurrentActivity();
         CrosswalkWebView crosswalkWebView = new CrosswalkWebView(context, _activity);
+
+        XWalkSettings settings =crosswalkWebView.getSettings();
+        settings.setDomStorageEnabled(true);//开启本地存储
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            settings.setAllowFileAccessFromFileURLs(false);
+            setAllowUniversalAccessFromFileURLs(crosswalkWebView, false);
+        }
+
+        // Fixes broken full-screen modals/galleries due to body height being 0.
+        crosswalkWebView.setLayoutParams(
+            new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // 启用cookies
+        XWalkCookieManager mCookieManager = new XWalkCookieManager();
+        mCookieManager.setAcceptCookie(true);
+        mCookieManager.setAcceptFileSchemeCookies(true);
+
         context.addLifecycleEventListener(crosswalkWebView);
         reactContext.addActivityEventListener(new XWalkActivityEventListener(crosswalkWebView));
         return crosswalkWebView;
@@ -95,17 +123,16 @@ public class CrosswalkWebViewGroupManager extends ViewGroupManager<CrosswalkWebV
       setUrl(view, BLANK_URL);
     }
 
-
     @ReactProp(name = "injectedJavaScript")
     public void setInjectedJavaScript (XWalkView view, @Nullable String injectedJavaScript) {
         ((CrosswalkWebView) view).setInjectedJavaScript(injectedJavaScript);
     }
-
+    // 启用postMessage?
     @ReactProp(name = "messagingEnabled")
     public void setMessagingEnabled(XWalkView view, boolean enabled) {
         ((CrosswalkWebView) view).setMessagingEnabled(enabled);
     }
-
+    // load的url
     @ReactProp(name = "url")
     public void setUrl (final CrosswalkWebView view, @Nullable final String url) {
         Activity _activity = reactContext.getCurrentActivity();
@@ -122,6 +149,44 @@ public class CrosswalkWebViewGroupManager extends ViewGroupManager<CrosswalkWebV
     @ReactProp(name = "localhost")
     public void setLocalhost (CrosswalkWebView view, Boolean localhost) {
         view.setLocalhost(localhost);
+    }
+    // 启用js?
+    @ReactProp(name = "javaScriptEnabled")
+    public void setJavaScriptEnabled(XWalkView view, boolean enabled) {
+        view.getSettings().setJavaScriptEnabled(enabled);
+    }
+    //禁止保存formData?
+    @ReactProp(name = "saveFormDataDisabled")
+    public void setSaveFormDataDisabled(XWalkView view, boolean disable) {
+        view.getSettings().setSaveFormData(!disable);
+    }
+    //启用本地存储？
+    @ReactProp(name = "domStorageEnabled")
+    public void setDomStorageEnabled(XWalkView view, boolean enabled) {
+        view.getSettings().setDomStorageEnabled(enabled);
+    }
+    // 媒体播放需要用户主动操作?
+    @ReactProp(name = "mediaPlaybackRequiresUserAction")
+    public void setMediaPlaybackRequiresUserAction(XWalkView view, boolean requires) {
+        view.getSettings().setMediaPlaybackRequiresUserGesture(requires);
+    }
+
+    @ReactProp(name = "allowUniversalAccessFromFileURLs")
+    public void setAllowUniversalAccessFromFileURLs(XWalkView view, boolean allow) {
+        view.getSettings().setAllowUniversalAccessFromFileURLs(allow);
+    }
+    // 设置用户代理
+    @ReactProp(name = "userAgent")
+    public void setUserAgent(XWalkView view, @Nullable String userAgent) {
+        if (userAgent != null) {
+            // TODO(8496850): Fix incorrect behavior when property is unset (uA == null)
+            view.getSettings().setUserAgentString(userAgent);
+        }
+    }
+
+    @ReactProp(name = "scalesPageToFit")
+    public void setScalesPageToFit(XWalkView view, boolean enabled) {
+        view.getSettings().setUseWideViewPort(!enabled);
     }
 
     @Override
